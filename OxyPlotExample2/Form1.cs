8d31ac1a64dac52e0e5087b5800c0dd2c1fd5377
wangkun <wangkun@cmiot.chinamobile.com>
2019-12-18 10:02:12 +0800

    整理代码；加入不同参数滤波器比较波形

--- OxyPlotExample2/Form1.cs
+++ OxyPlotExample2/Form1.cs
@@ -14,13 +14,13 @@ namespace OxyPlotExample2
     {
         private FrameDecoder m_frameDecoder = new FrameDecoder();
         private FlowSensor m_flowSensor = new FlowSensor();
-        private KalmanFilter m_kalmanFilter = new KalmanFilter(0.001f, 0.3f, 1.0f, 0);
+        private KalmanFilter m_kalmanFilter = new KalmanFilter(0.01f/*Q*/, 0.1f/*R*/, 1.0f/*P*/, 0);
+        private KalmanFilter m_kalmanFilter2 = new KalmanFilter(0.01f/*Q*/, 1.0f/*R*/, 10.0f/*P*/, 0);
         private LinearAxis m_xAxis; // X轴
         private LinearAxis m_yAxis; // Y轴
         private int m_X = 0;
 
         private PlotModel m_plotModel;
-        private Random rand = new Random(); // 用来生成随机数
 
         public Form1()
         {
@@ -72,7 +72,7 @@ namespace OxyPlotExample2
                 StrokeThickness = 1,
                 MarkerSize = 1,
                 MarkerStroke = OxyColors.DarkGreen,
-                MarkerType = MarkerType.Diamond,
+                MarkerType = MarkerType.Circle,
                 Title = "Presure"
             };
             m_plotModel.Series.Add(series);
@@ -84,7 +84,7 @@ namespace OxyPlotExample2
                 MarkerSize = 1,
                 MarkerStroke = OxyColors.DarkRed,
                 MarkerType = MarkerType.Circle,
-                Title = "AVG Filter Presure"
+                Title = "Filter1"
             };
             m_plotModel.Series.Add(series);
 
@@ -92,92 +92,41 @@ namespace OxyPlotExample2
             {
                 Color = OxyColors.Blue,
                 StrokeThickness = 1,
-                MarkerSize = 2,
+                MarkerSize = 1,
                 MarkerStroke = OxyColors.DarkBlue,
-                MarkerType = MarkerType.Triangle,
-                Title = "MID Filter Presure"
+                MarkerType = MarkerType.Circle,
+                Title = "Filter2"
             };
             m_plotModel.Series.Add(series);
 
             plotView1.Model = m_plotModel;
 
-#if false
-            Task.Factory.StartNew(() =>
-            {
-                int[] filterBuf = new int[9];
-                int filterCnt = 0;
-
-                StreamReader mysr = new StreamReader("H:\\CSharpWork\\OxyPlotExample2\\OxyPlotExample2\\data.csv", System.Text.Encoding.Default);
-                string strData = mysr.ReadToEnd();
-                mysr.Close();
-                mysr.Dispose();
-
-                string[] strDataArray = strData.Split(new char[] {','});
-                foreach (var strVal in strDataArray)
-                {
-                    if (String.Empty == strVal)
-                    {
-                        continue;
-                    }
-                    int val = Convert.ToInt32(strVal);
-                    var delta = 1000 / 1000;
-                    var date = DateTime.Now;
-                    m_plotModel.Axes[0].Maximum = DateTimeAxis.ToDouble(date.AddSeconds(delta / 1000));
-
-                    var lineSer1 = plotView1.Model.Series[0] as LineSeries;
-                    lineSer1.Points.Add(new DataPoint(DateTimeAxis.ToDouble(date), val));
-
-                    filterBuf[filterCnt++] = val;
-                    if (filterCnt >= filterBuf.Length)
-                    {
-                        Array.Sort(filterBuf);
-
-                        int sumVal = 0;
-                        for (int i = 1; i < filterBuf.Length - 1; i++)
-                        {
-                            sumVal += filterBuf[i];
-                        }
-                        int avgVal = sumVal / (filterBuf.Length - 2);
-                        var lineSer2 = plotView1.Model.Series[1] as LineSeries;
-                        lineSer2.Points.Add(new DataPoint(DateTimeAxis.ToDouble(date), avgVal));
-
-                        int midVal = filterBuf[filterBuf.Length / 2];
-                        var lineSer3 = plotView1.Model.Series[2] as LineSeries;
-                        lineSer3.Points.Add(new DataPoint(DateTimeAxis.ToDouble(date), midVal));
-
-                        filterCnt = 0;
-                    }
-
-                    m_plotModel.InvalidatePlot(true);
-
-                    Thread.Sleep(delta);
-                }
-            });
-#else
-#if false
+#if true
+            /* 通过文件获取数据 */
             m_frameDecoder.WaveDataRespRecved += new FrameDecoder.WaveDataRecvHandler((byte channel, double value) => {
                 Console.WriteLine($"WaveDataRespRecved: {channel} {value}");
-                var delta = 1000 / 100;
-                var date = DateTime.Now;
-                m_plotModel.Axes[0].Maximum = DateTimeAxis.ToDouble(date.AddSeconds(delta / 1000));
+                m_plotModel.Axes[0].Maximum = m_X + 1;
 
                 var lineSer1 = plotView1.Model.Series[0] as LineSeries;
-                lineSer1.Points.Add(new DataPoint(DateTimeAxis.ToDouble(date), value));
+                lineSer1.Points.Add(new DataPoint(m_X, value));
 
                 var lineSer2 = plotView1.Model.Series[1] as LineSeries;
-                lineSer2.Points.Add(new DataPoint(DateTimeAxis.ToDouble(date), m_kalmanFilter.Input((float)value)));
+                lineSer2.Points.Add(new DataPoint(m_X, m_kalmanFilter.Input((float)value)));
+
+                var lineSer3 = plotView1.Model.Series[2] as LineSeries;
+                lineSer3.Points.Add(new DataPoint(m_X, m_kalmanFilter2.Input((float)value)));
+
+                m_X++;
 
                 m_plotModel.InvalidatePlot(true);
             });
 
             Task.Factory.StartNew(() =>
             {
-                StreamReader mysr = new StreamReader("H:\\CSharpWork\\OxyPlotExample2\\OxyPlotExample2\\2lPERS1.txt", System.Text.Encoding.Default);
+                StreamReader mysr = new StreamReader("H:\\CSharpWork\\OxyPlotExample2\\OxyPlotExample2\\2LPERS.txt", System.Text.Encoding.Default);
                 string strData = mysr.ReadToEnd();
                 mysr.Close();
                 mysr.Dispose();
-
-                var delta = 1000 / 100;
 
                 string[] strDataArray = strData.Split(new char[] { ' ' });
                 foreach (var strVal in strDataArray)
@@ -189,11 +138,10 @@ namespace OxyPlotExample2
                     byte val = Convert.ToByte(strVal, 16);
 
                     m_frameDecoder.FrameDecodeInput(val);
-
-                    Thread.Sleep(delta);
                 }
             });
 #else
+            /* 通过传感器获取数据 */
             m_flowSensor.m_frameDecoder.WaveDataRespRecved += new FrameDecoder.WaveDataRecvHandler((byte channel, double value) => {
                 Console.WriteLine($"WaveDataRespRecved: {channel} {value}");
 
@@ -213,6 +161,13 @@ namespace OxyPlotExample2
                     lineSer2.Points.RemoveAt(0);
                 }
 
+                var lineSer3 = plotView1.Model.Series[2] as LineSeries;
+                lineSer3.Points.Add(new DataPoint(m_X, m_kalmanFilter2.Input((float)value)));
+                if (lineSer3.Points.Count > 2000)
+                {
+                    lineSer3.Points.RemoveAt(0);
+                }
+
                 m_X++;
 
                 m_plotModel.InvalidatePlot(true);
@@ -223,8 +178,8 @@ namespace OxyPlotExample2
             SendCmd("[ADC_START]");
             SendCmd("[ADC_CAL]");
 #endif
-#endif
         }
+
         private async void SendCmd(string cmd)
         {
             string cmdResp = await m_flowSensor.ExcuteCmdAsync(cmd, 2000);
